672fa13069272d6534ddbf26e3e97f5c2ce002ba
WeiJunYan <1956254031@qq.com>
2020-04-21 22:31:57 +0800

    提交

---
+++ js/one.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('#ah-tab-content-game .up a').mouseenter(function () {
+        $(this).css('opacity', 1);
+        $(this).siblings().css('opacity', 0.5);
+    })
+    $('#ah-tab-content-game .up a').mouseleave(function () {
+        $('li').css('opacity', 1);
+    })
+
+    $('#ah-tab-content-game .down a').mouseenter(function () {
+        $(this).css('opacity', 1);
+        $(this).siblings().css('opacity', 0.5);
+    })
+    $('#ah-tab-content-game .down a').mouseleave(function () {
+        $('li').css('opacity', 1);
+      })
+
+    
+})
